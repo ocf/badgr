7431d8e8c4f748344924d30e1f579de5d4d2750a
Nikhil Jha <njha@ocf.berkeley.edu>
2020-08-18 17:34:33 -0700

    :sparkles: fix theming

--- etc/environment.prod.ts
+++ etc/environment.prod.ts
@@ -6,6 +6,40 @@ export const environment: BadgrEnvironment = {
 			api: {
 				baseUrl: "//badgr-api.ocf.berkeley.edu",
 			},
+			help: {
+				email: "help@ocf.berkeley.edu"
+			},
+			features: {
+				alternateLandingRedirect: false
+			},
+			googleAnalytics: {
+				trackingId: null
+			},
+			theme: {
+				serviceName: "Badges", /* lol */
+				welcomeMessage: `### Welcome!`,
+				alternateLandingUrl: null,
+				showPoweredByBadgr: false,
+				showApiDocsLink: true,
+				termsOfServiceLink: null,
+				termsHelpLink: null,
+				privacyPolicyLink: null,
+				providedBy: {
+					name: "Open Computing Facility",
+					url: "https://ocf.berkeley.edu/"
+				},
+				logoImg: {
+					small: require("../../../node_modules/@concentricsky/badgr-style/dist/images/logo.svg") as string,
+					desktop: require("../../../node_modules/@concentricsky/badgr-style/dist/images/logo.svg") as string,
+				},
+				favicons: [],
+				useColorNavbar: true,
+				cssCustomProps: {
+					'--color-interactive1' : '#134074',
+					'--color-interactive2' : 'rgb(20,20,20)',
+					'--color-interactive2alpha50' : 'rgba(20,20,20,0.5)',
+				}
+			}
 		}
 	}
 ;
